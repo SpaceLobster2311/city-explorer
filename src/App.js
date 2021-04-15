66cb2d59cb66db684baf52a9d505a323e7fac915
Alexander Williams <alex.williams2311@gmail.com>
2021-04-14 18:33:59 -0700

    lab06 complete

--- src/App.js
+++ src/App.js
@@ -13,18 +13,23 @@ class App extends React.Component {
 
     this.state ={
       citySearched: '',
-      cityData: {}
+      cityData: {},
     }
   }
   handleformSubmit = async (event) => {
     event.preventDefault();
     console.log(this.state.citySearched)
-    let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.citySearched}&format=json`);
-    console.log(cityData);
-    let importantCity = cityData.data[0];
-    this.setState({
-      cityData: importantCity,
-    });
+    try{
+      let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.citySearched}&format=json`);
+      console.log(cityData);
+      let importantCity = cityData.data[0];
+      this.setState({
+        cityData: importantCity,
+      });
+    } catch(err) {
+      console.log(err);
+      this.setState({error: err.message})
+    }
   }
   render(){
     return(
@@ -37,9 +42,11 @@ class App extends React.Component {
           </Form.Group>
           <Button variant ="primary" type ="submit">Explore</Button>
         </Form>
+        {this.state.error ? <h3>{this.state.error}</h3> : ''}
         {this.state.cityData.lat ? <Jumbotron>
           <h3>{this.state.cityData.display_name}</h3>
           <h5>{this.state.cityData.lat}, {this.state.cityData.lon}</h5>
+          <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=13`} alt={`Map of ${this.state.cityData.display_name}`} />
         </Jumbotron> : ''}
       </>
     );
